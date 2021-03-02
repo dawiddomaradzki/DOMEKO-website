d9d3211addbf454acaba355aee32c77a494d07e6
Dawid Domaradzki <dawid-kun@wp.pl>
2021-03-02 18:52:14 +0100

    fix: margin & padding

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,4 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('Cześć, miło że zajrzałeś 🤠 nazywam się Dawid Domaradzki a wyświetlana strona to mój projekt. Zapraszam https://dawiddomaradzki.github.io/')
