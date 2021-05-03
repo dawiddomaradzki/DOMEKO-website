a2636e5ec2ee8a921fa88da1633f37be5702b663
Dawid Domaradzki <dawid-kun@wp.pl>
2021-05-03 12:40:15 +0200

    fix: jquery dropdown navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,20 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('Cześć, miło że zajrzałeś 🤠 nazywam się Dawid Domaradzki a wyświetlana strona to mój projekt. Zapraszam https://dawiddomaradzki.github.io/')
+console.log(
+  "Cześć, miło że zajrzałeś 🤠 nazywam się Dawid Domaradzki a wyświetlana strona to mój projekt. Zapraszam https://dawiddomaradzki.github.io/"
+);
+
+$(document).ready(function () {
+  $(".click").click(function (event) {
+    event.stopPropagation();
+    $(".showup").slideToggle("fast");
+  });
+  $(".showup").on("click", function (event) {
+    event.stopPropagation();
+  });
+});
+
+$(document).on("click", function () {
+  $(".showup").hide();
+});
